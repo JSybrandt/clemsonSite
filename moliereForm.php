78b89dca36e4a99d5e6c0c40d2e2c5a528596c8d
= <=>
2017-02-22 15:55:22 -0500

    made site

--- moliereForm.php
+++ moliereForm.php
@@ -6,19 +6,34 @@ include 'depends.php';
 </head>
 <body>
 <?php
-include 'navbar.php';
+include 'header.php';
 ?>
-<h2>Request Moliere Query</h2>
-<p>
-The following form will request information from the MOLIERE Biometical Hypothesis Generation System. Please allow at least 24 hours for us to respond to your query. If you have any questions, please contact us at jsybran (at) clemson (dot) edu.
-</p>
-<form method="post" action="/actions/moliereFormAction.php">
+<div class="w3-card-4 card">
+  <header class="w3-container purple">
+    <h1>Request Moliere Query</h1>
+  </header>
+  <div class="w3-container">
 
-Name: <input type="text" name="name">
-Organization: <input type="text" name="organization">
-E-Mail: <input type="text" name="email">
-Keyword ID 1: <input type="text" name="keyword1">
-Keyword ID 2: <input type="text" name="keyword2">
-<input type="submit" name="submit" value="Submit">
-</form>
+<p>The following form will request information from the MOLIERE Biometical Hypothesis Generation System. Please allow at least 24 hours for us to respond to your query. If you have any questions, please contact us at jsybran (at) clemson (dot) edu.</p>
+
+    <form class="moliereForm" method="post">
+    <table>
+    <tr><td>Name:</td>
+        <td><input type="text" name="name"></td></tr>
+    <tr><td>Organization:</td>
+        <td> <input type="text" name="organization"></td></tr>
+    <tr><td>E-Mail:</td>
+        <td> <input type="text" name="email"></td></tr>
+    <tr><td>Keyword ID 1:</td>
+        <td> <input type="text" name="keyword1"></td></tr>
+    <tr><td>Keyword ID 2:</td>
+        <td> <input type="text" name="keyword2"></td></tr>
+    </table>
+    <input type="submit" name="submit" value="Submit">
+    </form>
+  </div>
+</div>
+
+<script src="js/ajaxForm.js"></script>
+
 </body>
--- navbar.php
+++ navbar.php
@@ -1,9 +1,10 @@
-
 <nav class="navbar navbar-default">
   <div class="container-fluid">
+<!--
     <div class="navbar-header">
       <a class="navbar-brand" href="/index.php">Justin Sybrandt</a>
     </div>
+-->
     <ul class="nav navbar-nav">
       <li class="active"><a href="/index.php">Home</a></li>
       <li><a href="http://justin.sybrandt.com">Blog</a></li>
