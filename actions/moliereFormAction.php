a836e87ebf689b8214e9b5af47051621e08b1cd4
= <=>
2017-02-22 17:26:44 -0500

    php works finally

--- actions/moliereFormAction.php
+++ actions/moliereFormAction.php
@@ -6,22 +6,24 @@ $email=htmlspecialchars($_POST["email"]);
 $keyword1=htmlspecialchars($_POST["keyword1"]);
 $keyword2=htmlspecialchars($_POST["keyword2"]);
 
-if(isset($name) && isset($email) && isset($keyword1) && isset($keyword2)){
+if($name && $email && $keyword1 && $keyword2){
 
-  mail("jsybran@clemson.edu","MOLIERE Query",$name);
+  //mail("jsybran@clemson.edu","MOLIERE Query",$name);
   $serverName = "sbxmysql.clemson.edu";
   $username = "jsybran";
   $password = "V*AEsJl|ah)i";
-  $conn = new mysqli($serverName, $username, $password);
+  $dbname = "jsybran";
+  $conn = new mysqli($serverName, $username, $password, $dbname);
+
   if($conn->connect_error){
     echo("AN ERROR HAPPENED");
     die("Conn failed" . $conn->connect_error);
   }
-  $sql = "INSERT INTO `MoliereQuery`(`Name`, `Organization`, `Email`, `Keyword 1`, `Keyword 2`) VALUES ($name,$organization,$email,$keyword1,$keyword2)";
-  $conn->exec($sql);
+  $sql = "INSERT INTO `MoliereQuery`(`Name`, `Organization`, `Email`, `Keyword 1`, `Keyword 2`) VALUES ('$name','$organization', '$email', '$keyword1', '$keyword2')";
+  $conn->query($sql);
   $conn->close();
 }
 else{
   echo "Must supply Name, E-Mail, and two keywords.";
-}
+  }
 ?>
